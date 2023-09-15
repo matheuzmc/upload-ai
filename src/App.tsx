eb35be9aa47541f06cbc02d4c46e9b259659d52c
Matheus Costa <matheusmassem@gmail.com>
2023-09-15 19:25:06 +0000

    Cabeçalho

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
+import { Github } from "lucide-react";
 import { Button } from "./components/ui/button";
+import { Separator } from "./components/ui/separator";
 
 export function App() {
   return (
-    <Button>teste</Button>
+    <div>
+      <div className="flex items-center justify-between px-6 py-3 border-b">
+        <h1 className="text-xl font-bold">upload.ai</h1>
+
+        <div className="flex items-center gap-3">
+          <span className="text-sm text-muted-foreground">
+            Desenvolvido no NLW da Rocketseat
+          </span>
+
+          <Separator orientation="vertical" className="h-6" />
+
+          <Button variant="outline">
+            <Github className="w-4 h-4 mr-2" />
+            Github
+          </Button>
+        </div>
+      </div>
+    </div>
   )
 }
 
