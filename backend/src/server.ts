b622520a7c994f0cf2d45ea1b1ba81c8f6eabc7e
Matheus Costa <matheusmassem@gmail.com>
2023-09-18 18:22:29 +0000

    criação da rota de geração de prompt

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,12 +3,14 @@ import { prisma } from './lib/prisma'
 import { getAllPromptsRoute } from './routes/get-all-prompts'
 import { uploadVideoRoute } from './routes/upload-video'
 import { createTranscriptionRoute } from './routes/create-transcription'
+import { generateAICompletionRoute } from './routes/generate-ai-completion'
  
 const app = fastify()
 
 app.register(getAllPromptsRoute)
 app.register(uploadVideoRoute)
 app.register(createTranscriptionRoute)
+app.register(generateAICompletionRoute)
 
 app.listen({
     port: 3333,
